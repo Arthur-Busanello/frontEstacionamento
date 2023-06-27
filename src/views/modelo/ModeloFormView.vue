aaf86afda81e70c9dae9cf66c8e337c21b7096df
ARTHUR BUSANELLO <tuti.busanello@hotmail.com>
2023-06-27 10:44:43 -0300

    tentando ainda

--- src/views/modelo/ModeloFormView.vue
+++ src/views/modelo/ModeloFormView.vue
@@ -4,7 +4,7 @@
     <h5 class="labeling">Nome do modelo</h5>
     <div class="input-group mb-3">
       <input type="text" class="form-control" v-if="form === undefined" v-model="modelo.nome" placeholder="nome" aria-label="Recipient's username" aria-describedby="button-addon2">
-      <input type="text" class="form-control" v-if="form === undefined" v-marca="this.marca" placeholder="id" aria-label="Recipient's username" aria-describedby="button-addon2">
+      <!-- <input type="text" class="form-control" v-if="form === undefined" v-marca="this.marca" placeholder="id" aria-label="Recipient's username" aria-describedby="button-addon2"> -->
       <button class="btn btn-outline-secondary" v-if="form === undefined" @click="onClickCadastrar()"  type="button" id="button-addon2">Adicionar</button>
       <router-link to="/modelo"><button v-if="form === undefined" class="btn btn-outline-secondary" type="button" id="button-addon2">voltar</button></router-link>
     </div>
@@ -15,7 +15,7 @@
   import { Modelo } from '@/models/ModeloModel';
   import { defineComponent } from 'vue';
   
-  export default defineComponent({
+  export default defineComponent({  
     name: 'ModeloFormulario',
     data() {
       return {
